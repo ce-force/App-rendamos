876e1af09d76f9f2ffabf0a320ce947c0ba72614
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2019-10-26 14:24:47 -0600

    [UPDATE] Getting  the response of the post method

--- app/src/main/java/com/ceforce/app_rendamos/MainActivity.java
+++ app/src/main/java/com/ceforce/app_rendamos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Response";
     Button loadApi, postReq;
+    private static final String[] respuesta = {""};
+      JSONObject answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +54,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     postRequest();
+                    Thread.sleep(1000);
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            answer.toString(),
+                            Toast.LENGTH_SHORT);
+
+                    toast.show();
                 } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
@@ -96,10 +107,10 @@ public class MainActivity extends AppCompatActivity {
         MediaType MEDIA_TYPE = MediaType.parse("application/json");
         String url = "http://192.168.128.23:7321/ApiServer/api/LogIn";
 
-        OkHttpClient client = new OkHttpClient();
-        final String json="";
 
-        JSONObject postdata = new JSONObject();
+        OkHttpClient client = new OkHttpClient();
+
+        final JSONObject postdata = new JSONObject();
         try {
             postdata.put("username", "1111");
             postdata.put("password", "Te$t1234");
@@ -128,15 +139,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
-                String mMessage = response.body().string();
+                 String mMessage = response.body().string();
+                try {
+                    answer=new JSONObject(mMessage);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                respuesta[0] = mMessage;
                 Log.e(TAG, mMessage);
+                Log.e("Response 2", respuesta[0]);
+
                 //Toast.makeText(getApplicationContext(),mMessage,Toast.LENGTH_SHORT).show();
             }
         });
-        Toast toast = Toast.makeText(getApplicationContext(),
-                "This is a message displayed in a Toast",
-                Toast.LENGTH_SHORT);
+        Log.e("FINAL","HOla como etas"+ respuesta[0]);
+//
+//        //Toast toast = Toast.makeText(getApplicationContext(),respuesta[0].substring(0,10)
+//                ,
+//                Toast.LENGTH_SHORT);
+//
+//        toast.show();
 
-        toast.show();
+
+
     }
 }
