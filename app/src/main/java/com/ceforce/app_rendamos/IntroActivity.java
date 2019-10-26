aa3819f9b9ea625e0f8bf46d524f186f72ecfed3
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-10-26 11:14:45 -0600

    [ADD] Intro Slider

---
+++ app/src/main/java/com/ceforce/app_rendamos/IntroActivity.java
@@ -0,0 +1,37 @@
+package com.ceforce.app_rendamos;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //   setContentView(R.layout.activity_app_intro_activity);
+        addSlide(AppIntroFragment.newInstance("First page","This is the First Page Description",R.drawable.logo03, ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance("Second page","This is the Second Page Description",R.drawable.logo03, ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance("Third page","This is the Third Page Description",R.drawable.logo03, ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary)));
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+    }
+
+}
