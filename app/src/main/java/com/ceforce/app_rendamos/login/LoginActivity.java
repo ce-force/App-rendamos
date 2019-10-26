0e832f73a86394c75a674eaa299449dd0095a167
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-10-26 11:47:06 -0600

    [UPDATE] Remove Register Activity

--- app/src/main/java/com/ceforce/app_rendamos/login/LoginActivity.java
+++ app/src/main/java/com/ceforce/app_rendamos/login/LoginActivity.java
@@ -1,9 +1,6 @@
 package com.ceforce.app_rendamos.login;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,24 +8,21 @@ import com.ceforce.app_rendamos.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView tvSignUp;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        tvSignUp = findViewById(R.id.tvSignUp);
 
 
-        tvSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intSignUp = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intSignUp);
-            }
-        });
+//        tvSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intSignUp = new Intent(LoginActivity.this,RegisterActivity.class);
+//                startActivity(intSignUp);
+//            }
+//        });
 
     }
 }
