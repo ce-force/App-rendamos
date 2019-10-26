3119bbbff498c930e4831d9b4fbbda9b410f9e77
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-10-26 12:10:56 -0600

    [UPDATE] Input Fields Validation

--- app/src/main/java/com/ceforce/app_rendamos/login/LoginActivity.java
+++ app/src/main/java/com/ceforce/app_rendamos/login/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.ceforce.app_rendamos.login;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,13 +11,16 @@ import com.ceforce.app_rendamos.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText id, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        id = findViewById(R.id.idInputField);
+        password = findViewById(R.id.passwordInputField);
 
 //        tvSignUp.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -25,4 +31,15 @@ public class LoginActivity extends AppCompatActivity {
 //        });
 
     }
+
+    public void validateInputFields(View view) {
+        if (id.getText().toString().isEmpty() || password.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Llene todos los campos.", Toast.LENGTH_LONG).show();
+        }
+        if(id.getText().toString().length() < 9 ){
+            Toast.makeText(this, "Su id debe tener longitud igual a 9.", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
 }
