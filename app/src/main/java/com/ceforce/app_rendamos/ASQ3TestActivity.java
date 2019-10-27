ab5a4ce98342098f607da08e144a352cf0c3457d
Juan <juanpo.alvarado@gmail.com>
2019-10-27 02:46:29 -0600

    [ADD] Test activity

---
+++ app/src/main/java/com/ceforce/app_rendamos/ASQ3TestActivity.java
@@ -0,0 +1,53 @@
+package com.ceforce.app_rendamos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+public class ASQ3TestActivity extends AppCompatActivity {
+    TextView tv1;
+    TableLayout T;
+    String[] Areas={
+            "Comunicación",
+            "Motora Fina",
+            "Motora Gruesa",
+            "Resolución de problemas",
+            "Socio-individual"
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asq3_test);
+
+        tv1=findViewById(R.id.textView);
+        T=findViewById(R.id.table);
+
+        for (int i=0;i<Areas.length;i++){
+            /*TableRow Row= new TableRow(this);
+            TextView Tv=new TextView(this);
+            Tv.setText(i+1);
+            RadioGroup Rg=new RadioGroup(this);
+            Row.addView(Tv);
+            for(int j=1;j<7;j++){
+                RadioButton Rb=new RadioButton(this);
+                Rg.addView(Rb);
+            }
+            Row.addView(Rg);
+            T.addView(Row);*/
+        }
+    }
+
+    public void onDate(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        ((DatePickerFragment) newFragment).setTv(tv1);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+        Log.d("STATUS", ""+ ((DatePickerFragment) newFragment).getDateString());
+    }
+}
