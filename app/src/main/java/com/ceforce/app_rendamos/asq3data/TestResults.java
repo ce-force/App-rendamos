a5097bd69416e2d66bbbbd9700852117ea425b77
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-10-27 08:41:21 -0600

    [UPDATE] Dynamically Layout

--- app/src/main/java/com/ceforce/app_rendamos/asq3data/TestResults.java
+++ app/src/main/java/com/ceforce/app_rendamos/asq3data/TestResults.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import com.ceforce.app_rendamos.R;
 
@@ -14,6 +17,7 @@ public class TestResults extends AppCompatActivity {
 
     int[] array;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +25,19 @@ public class TestResults extends AppCompatActivity {
 
         array = getIntent().getIntArrayExtra("result");
     }
+
+    public void createRows() {
+        TableLayout tl = (TableLayout) findViewById(R.id.table);
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+        Button b = new Button(this);
+        b.setText("Dynamic Button");
+        b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        /* Add Button to row. */
+        tr.addView(b);
+        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+    }
+
 }
 
