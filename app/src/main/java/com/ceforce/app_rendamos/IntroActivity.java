db219b1466878ff31ae0f197cc33f16117f8f2d4
Juan <juanpo.alvarado@gmail.com>
2019-10-26 19:37:07 -0600

    [UPDATE] Final version 2

--- app/src/main/java/com/ceforce/app_rendamos/IntroActivity.java
+++ app/src/main/java/com/ceforce/app_rendamos/IntroActivity.java
@@ -1,10 +1,15 @@
 package com.ceforce.app_rendamos;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -24,6 +29,23 @@ public class IntroActivity extends AppIntro {
             Intent intSignUp = new Intent(this, MainActivity.class);
             startActivity(intSignUp);
         }
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+
+        }
+        else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setMessage("Revise su conexión a wifi")
+                    .setTitle("Wifi no detectado");
+
+            AlertDialog dialog = builder.create();
+
+            dialog.show();
+        }
+
         addSlide(AppIntroFragment.newInstance("","Paso 1. Inicio de sesión",R.drawable.logo03, ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary)));
         addSlide(AppIntroFragment.newInstance("","Paso 2.",R.drawable.logo03, ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary)));
         addSlide(AppIntroFragment.newInstance("","Paso 3",R.drawable.logo03, ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary)));
